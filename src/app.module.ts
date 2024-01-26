80bfff058ee65466dd94ce62a9fd0f99cf9e848c
joaovitorscr <98563963+joaovitorscr@users.noreply.github.com>
2024-01-26 14:25:16 -0300

    FEAT: Authentication Module and CORS configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
+import { AuthModule } from './authentication/auth.module'
 import { ProductModule } from './product/product.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
